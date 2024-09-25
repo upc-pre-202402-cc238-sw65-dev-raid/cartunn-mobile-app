4d3b1e2c4c3ae28555fc69aa4b884f035cc90b26
abrancitouu <abrahamayquipa2000@gmail.com>
2024-09-24 20:31:48 -0500

    feat: add the upload item page

---
+++ lib/components/button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Color buttonColor;
+  final Color textColor;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.buttonColor,
+    required this.textColor,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor,
+      ),
+      child: Text(
+        text,
+        style: TextStyle(color: textColor),
+      ),
+    );
+  }
+}
---
+++ lib/views/editItem/notifyClient/notify_client.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotifyClientPage extends StatelessWidget {
+  const NotifyClientPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Notify Client Page',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}

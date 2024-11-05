b14ed11f35accb05574c5fd7dae08828f750ff44
MarianaAlexandra05 <marianaalexandrachm14@gmail.com>
2024-11-04 19:50:41 -0500

    feat: update button styles in Product Refunds bottom sheet

--- lib/views/manageReturns/manage_returns.dart
+++ lib/views/manageReturns/manage_returns.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'entity/manage_return.dart'; // Asegúrate de que este archivo existe y tiene la clase ProductRefund.
+import 'package:cartunn/components/draggable_sheet_component.dart';
 
 class ManageReturnsPage extends StatefulWidget {
   const ManageReturnsPage({super.key});
@@ -19,14 +21,12 @@ class _ManageReturnsPageState extends State<ManageReturnsPage> {
   }
 
   Future<void> fetchProductRefunds() async {
-    final response = await http
-        .get(Uri.parse('https://cartunn.up.railway.app/api/v1/product-refund'));
+    final response = await http.get(Uri.parse('https://cartunn.up.railway.app/api/v1/product-refund'));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       setState(() {
-        productRefunds = List<ProductRefund>.from(
-            data.map((x) => ProductRefund.fromJson(x)));
+        productRefunds = List<ProductRefund>.from(data.map((x) => ProductRefund.fromJson(x)));
       });
     } else {
       throw Exception('Failed to load product refunds');
@@ -63,227 +63,131 @@ class _ManageReturnsPageState extends State<ManageReturnsPage> {
     }
   }
 
-  void _openRefundDetailModal(ProductRefund refund) {
-    String selectedStatus = refund.status;
-
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      builder: (context) {
-        return Padding(
-          padding: EdgeInsets.only(
-            left: 32,
-            right: 32,
-            top: 32,
-            bottom: MediaQuery.of(context).viewInsets.bottom + 32,
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Wrap(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      refund.title,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF5766F5),
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Description:',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey[600],
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Text(
-                        refund.description,
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Status:',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey[600],
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    DropdownButton<String>(
-                      value: selectedStatus,
-                      items: const [
-                        DropdownMenuItem(
-                          value: 'Rejected',
-                          child: Text('Rejected'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Processing',
-                          child: Text('Processing'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Processed',
-                          child: Text('Processed'),
-                        ),
-                      ],
-                      onChanged: (value) {
-                        setState(() {
-                          selectedStatus = value!;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 24),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              await _updateRefundStatus(refund, selectedStatus);
-                              Navigator.of(context).pop();
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFF5766F5),
-                              padding: const EdgeInsets.symmetric(vertical: 16),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                            child: const Text(
-                              'SAVE',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            style: ElevatedButton.styleFrom(
-                              padding: const EdgeInsets.symmetric(vertical: 16),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                            child: const Text(
-                              'CLOSE',
-                              style: TextStyle(
-                                color: Color(0xFF5766F5),
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
+  Color _getStatusColor(String status) {
+    switch (status) {
+      case 'Rejected':
+        return Colors.red;
+      case 'Processing':
+        return Colors.orange;
+      case 'Processed':
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
   }
 
-  Icon _buildStatusIcon(String status) {
-    return Icon(
-      status == 'Processed'
-          ? Icons.check_circle
-          : status == 'Processing'
-              ? Icons.access_time
-              : Icons.cancel,
-      color: status == 'Processed'
-          ? Colors.green
-          : status == 'Processing'
-              ? Colors.orange
-              : Colors.red,
-    );
+  IconData _getStatusIcon(String status) {
+    switch (status) {
+      case 'Rejected':
+        return Icons.cancel;
+      case 'Processing':
+        return Icons.timelapse;
+      case 'Processed':
+        return Icons.check_circle;
+      default:
+        return Icons.info;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Manage Refunds',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Product Refunds',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
             ),
           ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: productRefunds.length,
-            itemBuilder: (context, index) {
-              final refund = productRefunds[index];
-              return GestureDetector(
-                onTap: () {
-                  _openRefundDetailModal(refund);
-                },
-                child: Card(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 16,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                refund.title,
-                                style: const TextStyle(
-                                  color: Color(0xFF5766F5),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: productRefunds.length,
+              itemBuilder: (context, index) {
+                final productRefund = productRefunds[index];
+                return GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (context) => GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Stack(
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: DraggableSheetComponent(
+                                child: ProductRefundDetailContent(
+                                  productRefund: productRefund,
+                                  onSave: (newStatus) {
+                                    _updateRefundStatus(productRefund, newStatus);
+                                  },
                                 ),
                               ),
-                              const SizedBox(height: 8),
-                              Text(
-                                refund.description,
-                                style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    elevation: 4,
+                    margin: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            productRefund.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            productRefund.description,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                _getStatusIcon(productRefund.status),
+                                color: _getStatusColor(productRefund.status),
                               ),
-                              const SizedBox(height: 8),
-                              Row(
-                                children: [
-                                  _buildStatusIcon(refund.status),
-                                  const SizedBox(width: 8),
-                                  Text(
-                                    refund.status,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ],
+                              const SizedBox(width: 8),
+                              Text(
+                                productRefund.status,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: _getStatusColor(productRefund.status),
+                                ),
+                                textAlign: TextAlign.center,
                               ),
                             ],
                           ),
@@ -291,33 +195,150 @@ class _ManageReturnsPageState extends State<ManageReturnsPage> {
                       ],
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
 
-class ProductRefund {
-  final int id;
-  final String title;
-  final String description;
-  String status;
+class ProductRefundDetailContent extends StatefulWidget {
+  final ProductRefund productRefund;
+  final ValueChanged<String> onSave;
 
-  ProductRefund({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.status,
+  const ProductRefundDetailContent({
+    super.key,
+    required this.productRefund,
+    required this.onSave,
   });
 
-  factory ProductRefund.fromJson(Map<String, dynamic> json) {
-    return ProductRefund(
-      id: json['id'],
-      title: json['title'],
-      description: json['description'],
-      status: json['status'],
+  @override
+  _ProductRefundDetailContentState createState() => _ProductRefundDetailContentState();
+}
+
+class _ProductRefundDetailContentState extends State<ProductRefundDetailContent> {
+  late String _selectedStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedStatus = widget.productRefund.status;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.productRefund.title,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Description',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey[600],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            widget.productRefund.description,
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Status',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey[600],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 4),
+          DropdownButton<String>(
+            value: _selectedStatus,
+            items: const [
+              DropdownMenuItem(
+                value: 'Rejected',
+                child: Text('Rejected'),
+              ),
+              DropdownMenuItem(
+                value: 'Processing',
+                child: Text('Processing'),
+              ),
+              DropdownMenuItem(
+                value: 'Processed',
+                child: Text('Processed'),
+              ),
+            ],
+            onChanged: (value) {
+              setState(() {
+                _selectedStatus = value!;
+              });
+            },
+          ),
+          const SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    widget.onSave(_selectedStatus);
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF5766F5),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'SAVE',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'CLOSE',
+                    style: TextStyle(
+                      color: Color(0xFF5766F5),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
